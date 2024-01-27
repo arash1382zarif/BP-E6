485002b0282c5d88e42ff6a485b264c2af8fdd55
arash <arash1382zarif@gmail.com>
2024-01-27 17:35:19 +0330

    refactored code

--- FararehJerry.py
+++ FararehJerry.py
@@ -1,40 +1,54 @@
-n = 0
-n = int(input())
-input_str = ''
-height = 0
-x = 0
+def get_input(prmopt):
+    """Get user input."""
+    return input(prmopt).strip()
 
-map = []
 
-    
-map.append("." * n)
+def create_empty_map(n):
+    """Create an empty map of size n."""
+    return ["." * n]
 
-map[0] = map[0][:0] + "*" + map[0][1:]
 
-while(True):
-    input_str = input()
-    input_str = input_str.strip()
-    if (input_str == 'B'):
-        height += 1
-        map.append(n * '.')
-    if (input_str == 'R'):
-        x += 1
-        if (x > n - 1):
-            x = n - 1
-    if (input_str == 'L'):
-        x -= 1
-        if (x < 0):
-            x = 0
-    
+def update_map(my_map, height, x):
+    """Update the map based on user input."""
+    my_map[height] = my_map[height][:x] + "*" + my_map[height][x + 1:]
 
-    map[height] = map[height][:x] + "*" + map[height][x+1:]
 
-    if (input_str == "END"):
-        break
+def print_map(my_map, height):
+    """Print the map up to the specified height."""
+    for i in range(height + 1):
+        print(" ".join(my_map[i]))
 
-for i in range(height + 1):
-    for j in range(n):
-        print(map[i][j], end=' ')
-    print()
-if (x != n -1):
-    print("There's no way out!")
+
+def main():
+    n = int(get_input(""))
+    height = 0
+    x = 0
+
+    my_map = create_empty_map(n)
+
+    while True:
+        input_str = get_input("")
+
+        if input_str == 'B':
+            height += 1
+            my_map.append(n * '.')
+
+        if input_str == 'R':
+            x = min(x + 1, n - 1)
+
+        if input_str == 'L':
+            x = max(x - 1, 0)
+
+        update_map(my_map, height, x)
+
+        if input_str == "END":
+            break
+
+    print_map(my_map, height)
+
+    if x != n - 1:
+        print("There's no way out!")
+
+
+if __name__ == "__main__":
+    main()
