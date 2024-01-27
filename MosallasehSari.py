d24c0499a14b40ed5cbe746d03a946a94990836b
arash <arash1382zarif@gmail.com>
2024-01-27 17:41:37 +0330

    refactored code

--- MosallasehSari.py
+++ MosallasehSari.py
@@ -1,8 +1,21 @@
-n = int(input())
+def get_int_input(prompt):
+    return int(input(prompt))
 
-for line in range(1, n + 1):  
-        C = 1
+
+def generate_pascal_triangle(n):
+    """Generate Pascal's Triangle up to the nth row."""
+    for line in range(1, n + 1):
+        coefficient = 1
         for i in range(1, line + 1):
-            print(C, end = " ")
-            C = int(C * (line - i) / i)
+            print(coefficient, end=" ")
+            coefficient = int(coefficient * (line - i) / i)
         print("")
+
+
+def main():
+    n = get_int_input("")
+    generate_pascal_triangle(n)
+
+
+if __name__ == "__main__":
+    main()
