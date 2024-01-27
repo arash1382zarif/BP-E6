c878e3855214af7d70612a58be66e7976c1a92cf
arash <arash1382zarif@gmail.com>
2024-01-27 17:18:47 +0330

    refactored code

--- DastGarmi.py
+++ DastGarmi.py
@@ -1,1 +1,10 @@
-print("Hello Python")
+#!/usr/bin/env python3
+# Author: Arash Zarif
+
+def greet():
+    """Print a friendly greeting."""
+    print("Hello Python")
+
+
+if __name__ == "__main__":
+    greet()
