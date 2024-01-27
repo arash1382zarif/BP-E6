40a68ac039c7b23368fec47c02dd2f389bace6f9
arash <arash1382zarif@gmail.com>
2024-01-27 17:44:49 +0330

    refactored code

--- TalehHoushmand.py
+++ TalehHoushmand.py
@@ -1,45 +1,43 @@
-def sum_of_numerators(n):
-    sum = 0
-    for i in range(1,n + 1):
-        if (n % i == 0):
-            sum += i
-    return sum
+def sum_of_divisors(n):
+    return sum(i for i in range(1, n + 1) if n % i == 0)
 
-def numberToBase(n, b):
+
+def number_to_base(n, base):
     if n == 0:
         return [0]
     digits = []
     while n:
-        digits.append(int(n % b))
-        n //= b
+        digits.append(int(n % base))
+        n //= base
     return digits[::-1]
 
-n = 0
-b = 0
-sum = 0
-flag = True
-while(True):
-    n, b = map(int,input().split())
 
-    if (n == -1 and b == -1):
-        break
+def main():
+    total_sum = 0
+    flag = True
 
-    if (b < 2 or b > 9):
-        print("invalid base!")
-        flag = False
-        break
+    while True:
+        n, b = map(int, input().split())
 
-    x = ''
+        if n == -1 and b == -1:
+            break
 
-    digits = numberToBase(sum_of_numerators(n), b)
+        if b < 2 or b > 9:
+            print("invalid base!")
+            flag = False
+            break
 
-    for i in range(len(digits)):
-        x += str(digits[i])
+        x = ''
+        digits = number_to_base(sum_of_divisors(n), b)
 
-    sum += int(x)
+        for digit in digits:
+            x += str(digit)
 
-if (flag):
-    print(sum) 
+        total_sum += int(x)
+
+    if flag:
+        print(total_sum)
 
 
-
+if __name__ == "__main__":
+    main()
