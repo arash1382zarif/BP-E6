45f1b229063632cf917036034c2d0ed77fa0e412
arash <arash1382zarif@gmail.com>
2024-01-27 17:29:15 +0330

    refactored code

--- MehmaniDrAfshar.py
+++ MehmaniDrAfshar.py
@@ -1,14 +1,42 @@
-right = int(input())
-left = int(input())
-binary_right = bin(right).replace("0b", "")
-binary_left = bin(left).replace("0b", "")
-binary_right = (32 - len(binary_right)) * "0" + binary_right
-binary_left = (32 - len(binary_left)) * "0" + binary_left
-binary_string = binary_left + binary_right
-n = int(input())
-for i in range(n):
-    index = int(input())
-    if binary_string[63-index] == "1":
-        print("yes")
-    else:
-        print("no")
+def get_int_input(prompt):
+    """Get integer input from the user."""
+    return int(input(prompt))
+
+
+def convert_to_binary_string(number):
+    """Convert an integer to a 32-bit binary string."""
+    binary_representation = bin(number)[2:]
+    return (32 - len(binary_representation)) * "0" + binary_representation
+
+
+def check_bit_at_index(binary_string, index):
+    """Check if the bit at a specific index in the binary string is '1'."""
+    return binary_string[63 - index] == "1"
+
+
+def main():
+    # Get input values
+    right = get_int_input("")
+    left = get_int_input("")
+
+    # Convert to binary strings
+    binary_right = convert_to_binary_string(right)
+    binary_left = convert_to_binary_string(left)
+
+    # Concatenate binary strings
+    binary_string = binary_left + binary_right
+
+    # Number of queries
+    n = get_int_input("")
+
+    # Check bits at specified indices
+    for _ in range(n):
+        index = get_int_input("")
+        if check_bit_at_index(binary_string, index):
+            print("yes")
+        else:
+            print("no")
+
+
+if __name__ == "__main__":
+    main()
