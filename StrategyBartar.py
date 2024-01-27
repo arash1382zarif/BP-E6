52292f7b52964f73e39e1eebe0b9fd96a9992fca
arash <arash1382zarif@gmail.com>
2024-01-27 17:31:16 +0330

    refactored code

--- StrategyBartar.py
+++ StrategyBartar.py
@@ -1,20 +1,34 @@
-inp = (input()).split(" ")
-a, b = int(inp[0]), int(inp[1])
-if a > b:
-   c = b
-   b = a
-   a = c
-   print('reverse order - amount: ', end='')
-else:
-   print('main order - amount: ', end='')
-count = 0
-for x in range(a, b+1):
-  if x>1:
-      yN = False
-      for i in range(2 , x):
-          if (x % i) == 0:
-            yN = True
-            break
-      if yN == False:
-        count+=1
-print(count)
+def get_input():
+    """Get space-separated integers as input."""
+    return map(int, input().split())
+
+
+def is_prime(num):
+    """Check if a number is prime."""
+    if num > 1:
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+    return False
+
+
+def main():
+    # Get input values
+    a, b = get_input()
+
+    # Ensure a is less than or equal to b
+    if a > b:
+        a, b = b, a
+        print('reverse order - amount: ', end='')
+    else:
+        print('main order - amount: ', end='')
+
+    # Count prime numbers in the range [a, b]
+    count = sum(1 for x in range(a, b + 1) if is_prime(x))
+
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
